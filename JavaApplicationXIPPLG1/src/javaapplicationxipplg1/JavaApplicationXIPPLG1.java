c5d181d366bbd9f22af2922a78441672be955f6a
iwankecebanget <buakarridwan@gmail.com>
2025-07-22 15:17:35 +0700

    Add files via upload

---
+++ JavaApplicationXIPPLG1/src/javaapplicationxipplg1/JavaApplicationXIPPLG1.java
@@ -0,0 +1,93 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplicationxipplg1;
+
+//    @SuppressWarnings("unused")
+
+import java.util.Scanner;
+import javax.swing.JOptionPane;
+
+//    private static Object JOptionPane;
+///**
+// *
+// * @author PC 18- LAB R1
+// */
+public class JavaApplicationXIPPLG1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+//        System.out.println("sule" + " " + "prikitiw");
+//        System.out.println("2" + " " + "5");
+//        System.out.println(2+5);
+//        System.out.println("angka" + " " + "5");
+//        System.out.println("2 + 5"+" = "+2+5);
+//        System.out.println("2 + 5"+" = "+(2+5));
+        // TODO code application logic here
+//
+//       String namaDepan = "";
+//        String namaBelakang = "";
+//        @SuppressWarnings("unused")
+//        Scanner input = new Scanner(System.in);
+//        System.out.println("namaDepan : ");
+//        //membaca input user
+//        namaDepan = input.next();
+//        System.out.println("namaBelakang : ");
+//        //membaca input user
+//        namaBelakang = input.next();
+//        System.out.println("Nama Saya adalah");
+//        System.out.println(namaDepan + " " + namaBelakang);
+
+//   String nama = JOptionPane.showInputDialog("Masukan nama:");
+//   System.out.println("Nama :" + nama);
+
+//    String a = JOptionPane.showInputDialog("Masukan Nilai untuk variabel a : ");
+//    
+//   String b = JOptionPane.showInputDialog("Masukan Nilai untuk variabel b : ");
+//   
+//    System.out.println("Hasil :" +(Integer.parseInt(a) * Integer.parseInt(b)));
+
+//       String nama = "Ridwan";
+//    String jurusan = "PPLG";
+//   JOptionPane.showMessageDialog(null, "Hallo" + nama + ",jurusan Kamu" + jurusan);
+   
+//    JOptionPane.showMessageDialog(
+//                   null, "Sebuah Informasi.", "Informasi",
+//                   JOptionPane.INFORMATION_MESSAGE);
+
+//     JOptionPane.showMessageDialog( 
+//                   null, "Sebuah Error !", "Error",
+//                    JOptionPane.ERROR_MESSAGE);
+
+//     JOptionPane.showMessageDialog(
+//                  null, "Sebuah Peringatan.", "Peringatan",
+//                   JOptionPane.WARNING_MESSAGE);
+
+//          JOptionPane.showMessageDialog(
+//                   null, "Sebuah Pertanyaan.", "Pertanyaan",
+//                    JOptionPane.QUESTION_MESSAGE);
+
+//                      JOptionPane.showMessageDialog(
+//                   null, "Sebuah Plain message.", "Plain message",
+//                   JOptionPane.QUESTION_MESSAGE);
+
+            int pertanyaan
+                    = JOptionPane.showConfirmDialog(null, "Pilih Yes atau No"
+                            + "?", "Pertanyaan"
+                                    + "",JOptionPane.YES_NO_OPTION);
+
+if (JOptionPane.YES_OPTION == pertanyaan) 
+                JOptionPane.showMessageDialog(null, "Kamu memilih Yes.");
+            else if (JOptionPane.NO_OPTION == pertanyaan) 
+                JOptionPane.showMessageDialog(null, "Kamu memilih No.");
+    }
+    
+} 
+
+            
+        
+
+
